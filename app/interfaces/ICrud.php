2b2d59e9417c91a5b82acd589ec95312e2a25bc6
rociobessio <rocibessio@gmail.com>
2023-11-05 20:53:28 -0300

    Modificacion en las interfaces

--- app/interfaces/ICrud.php
+++ app/interfaces/ICrud.php
@@ -6,7 +6,7 @@
     interface ICrud{
         public static function crear($obj);
 	    public static function obtenerTodos();
-	    public static function obtenerUno($valor);
+	    public static function obtenerUno($id);
 	    public static function modificar($obj);
-	    public static function borrar($obj);
+	    public static function borrar($id);
     }
